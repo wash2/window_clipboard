6ca3cc3d4c221d34a4c385957bd3fd8be9ad48e5
Jeremy Soller <jackpot51@gmail.com>
2024-04-13 08:59:37 -0600

    Stub DndProvider for dummy platform

--- src/platform/dummy.rs
+++ src/platform/dummy.rs
@@ -1,6 +1,9 @@
-use crate::ClipboardProvider;
+use crate::{dnd::DndProvider, ClipboardProvider};
 
+use dnd::{DndAction, DndDestinationRectangle, DndSurface, Icon};
+use mime::{AllowedMimeTypes, AsMimeTypes};
 use raw_window_handle::HasDisplayHandle;
+use std::borrow::Cow;
 
 pub struct Clipboard;
 
@@ -20,6 +23,45 @@ impl ClipboardProvider for Clipboard {
         _contents: String,
     ) -> Result<(), Box<dyn std::error::Error>> {
         Err(Box::new(Error::Unimplemented))
+    }
+}
+
+impl DndProvider for Clipboard {
+    fn init_dnd(
+        &self,
+        _tx: Box<dyn dnd::Sender<DndSurface> + Send + Sync + 'static>,
+    ) {
+    }
+
+    fn start_dnd<D: AsMimeTypes + Send + 'static>(
+        &self,
+        _internal: bool,
+        _source_surface: DndSurface,
+        _icon_surface: Option<Icon>,
+        _content: D,
+        _actions: DndAction,
+    ) {
+    }
+
+    fn end_dnd(&self) {}
+
+    fn register_dnd_destination(
+        &self,
+        _surface: DndSurface,
+        _rectangles: Vec<DndDestinationRectangle>,
+    ) {
+    }
+
+    fn set_action(&self, _action: DndAction) {}
+
+    fn peek_offer<D: AllowedMimeTypes + 'static>(
+        &self,
+        _mime_type: Option<Cow<'static, str>>,
+    ) -> std::io::Result<D> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "DnD not supported",
+        ))
     }
 }
 
