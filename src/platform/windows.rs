7a33fb2299c49336555f5c7b479019c62d837b2f
Ashley Wulber <ashley@system76.com>
2024-06-19 12:37:26 -0400

    fix: stubs for various platforms

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,14 +1,54 @@
 use crate::ClipboardProvider;
 
+use crate::dnd::DndProvider;
 use clipboard_win::{get_clipboard_string, set_clipboard_string};
+use dnd::{DndAction, DndDestinationRectangle, DndSurface, Icon};
+use mime::{AllowedMimeTypes, AsMimeTypes};
 use raw_window_handle::HasDisplayHandle;
-
-use std::error::Error;
+use std::{borrow::Cow, error::Error};
 
 pub fn connect<W: HasDisplayHandle>(
     _window: &W,
 ) -> Result<Clipboard, Box<dyn Error>> {
     Ok(Clipboard)
+}
+impl DndProvider for Clipboard {
+    fn init_dnd(
+        &self,
+        _tx: Box<dyn dnd::Sender<DndSurface> + Send + Sync + 'static>,
+    ) {
+    }
+
+    fn start_dnd<D: AsMimeTypes + Send + 'static>(
+        &self,
+        _internal: bool,
+        _source_surface: DndSurface,
+        _icon_surface: Option<Icon>,
+        _content: D,
+        _actions: DndAction,
+    ) {
+    }
+
+    fn end_dnd(&self) {}
+
+    fn register_dnd_destination(
+        &self,
+        _surface: DndSurface,
+        _rectangles: Vec<DndDestinationRectangle>,
+    ) {
+    }
+
+    fn set_action(&self, _action: DndAction) {}
+
+    fn peek_offer<D: AllowedMimeTypes + 'static>(
+        &self,
+        _mime_type: Option<Cow<'static, str>>,
+    ) -> std::io::Result<D> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "DnD not supported",
+        ))
+    }
 }
 
 pub struct Clipboard;
